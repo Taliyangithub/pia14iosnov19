f7f1a29ae9121bbab4c670e37f71a9731e98aecc
Priya Taliyan <chaudharypia94@gmail.com>
2025-11-28 12:20:11 +0100

    Initial Commit

---
+++ pia14iosnov19/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pia14iosnov19
+//
+//  Created by Priya Taliyan on 2025-11-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pia14iosnov19/pia14iosnov19App.swift
@@ -0,0 +1,17 @@
+//
+//  pia14iosnov19App.swift
+//  pia14iosnov19
+//
+//  Created by Priya Taliyan on 2025-11-28.
+//
+
+import SwiftUI
+
+@main
+struct pia14iosnov19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
